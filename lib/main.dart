14cb4412375e839e6548e5af340d0e6c9193e6e9
MartinSteinmayer <martin.j.steinmayer@gmail.com>
2023-11-03 19:02:57 +0100

    styled start button

--- lib/main.dart
+++ lib/main.dart
@@ -43,59 +43,65 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  bool _isTimerRunning = false;
 
-  void _incrementCounter() {
+  // Assuming your Timer component has a method to start the timer
+  void _startTimer() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+      _isTimerRunning = true;
+      // Add the logic or method call to start the timer here
+      // For example: timer.start();
+    });
+  }
+
+  void _terminateTimer() {
+    setState(() {
+      _isTimerRunning = false;
+      // Add the logic or method call to terminate the timer here
+      // For example: timer.terminate();
+    });
+  }
+
+  _incrementCounter() {
+    setState(() {
       _counter++;
     });
   }
 
+  
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Timer(),
+            const Timer(), // Your Timer component should be here
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            ElevatedButton(
+              onPressed: _isTimerRunning ? _terminateTimer : _startTimer,
+              child: Text(_isTimerRunning ? 'Stop Timer' : 'Start Timer'),
+              style: ElevatedButton.styleFrom(
+                primary: _isTimerRunning ? Colors.red : Colors.green, // Background color
+                onPrimary: Colors.white, // Text Color
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30), // Rounded edges
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20), // Button padding
+                textStyle: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+                elevation: 10, // Shadow elevation
+              ),
             ),
           ],
         ),
@@ -104,7 +110,8 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
+
